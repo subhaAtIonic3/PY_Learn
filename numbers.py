874900b5140e49dbfd3e252fbc1715e8f4c76096
Subhrajyoti <subha.ionic3@gmail.com>
2020-08-01 03:24:56 +0530

    string and numbers basics

---
+++ numbers.py
@@ -0,0 +1,24 @@
+print(2 ** 3)
+print(0.1 + 0.2 - 0.3)
+print(1/2)
+print(1 % 2)
+
+number = 20
+number_two = 10
+number = 40
+print(number + number_two)
+
+new_number = 20
+print(type(new_number))
+
+new_number = 20.22
+print(type(new_number))
+
+num_a = 10
+num_b = 20
+
+num_b = num_a + num_b
+num_a = num_b - num_a
+num_b = num_b - num_a
+
+print("Swap numbers are =>", num_a, num_b)
---
+++ strings.py
@@ -0,0 +1,17 @@
+print("hello \n world")
+print("hello \nworld")
+print("hello \t world")
+
+print(len("Check length"))
+
+my_string = "Hello World"
+print(my_string[9])  # fwd index position
+print(my_string[-2])  # bwd index postion
+
+# print(my_string[2:])
+# print(my_string[:5])
+print("Slice String =>", my_string[2: 9])
+
+print("Step size =>", my_string[::2])
+print("Combine Step Size =>", my_string[1:9:3])
+print("Reverse a string => ", my_string[::-1])
