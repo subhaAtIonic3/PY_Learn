eb797ce42c35b80f73e1b36e1415e4d8dabd109d
Subhrajyoti <subha.ionic3@gmail.com>
2020-08-13 13:22:30 +0530

    Learn war card game

---
+++ test_cap.py
@@ -0,0 +1,19 @@
+import unittest
+import cap
+
+
+class TestCap(unittest.TestCase):
+
+    def test_one_word(self):
+        text = "python"
+        result = cap.cap_text(text)
+        self.assertEqual(result, "Python")
+
+    def test_multi_word(self):
+        text = "multi python"
+        result = cap.cap_text(text)
+        self.assertEqual(result, "Multi Python")
+
+
+if __name__ == "__main__":
+    unittest.main()
